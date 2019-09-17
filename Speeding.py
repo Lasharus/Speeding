fb40640d2b5cc766f4d70ba626d1795bcd946a5b
Jelmer van der Schaaf <jelmer_van_der_schaaf@hotmail.com>
2019-09-17 11:04:09 +0200

    fixed code speeding.py

--- Speeding.py
+++ Speeding.py
@@ -2,14 +2,14 @@ class HighSpeed(object):
     
     def Speeding(self, x):
         if x <= 50:
-            return "Pass"
             print("Pass")
+            return "Pass"
         elif x <= 55:
-            return "10 euros"
             print("10 euros")
+            return "10 euros"
         elif x <= 60:
-            return "20 euros"
             print("20 euros")
+            return "20 euros"
         elif x <= 65:
             print("30 euros")
             return "30 euros"
@@ -23,7 +23,7 @@ class HighSpeed(object):
             print("60 euros")
             return "60 euros"
         else:
-            return "Goodbye License"
             print("Goodbye License")
+            return "Goodbye License"
             
 
